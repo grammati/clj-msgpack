2221ca2665ad821cbc5484820858371489e98a18
Chris Perkins <chrisperkins99@gmail.com>
2011-10-01 17:54:44 -0400

    clojure 1.3

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject clj-msgpack "0.1.0"
   :description "Messagepack serialization library for Clojure."
   :dependencies [
-    [org.clojure/clojure "1.2.1"]
+    [org.clojure/clojure "1.3.0"]
     [org.msgpack/msgpack "0.5.2-devel"]
     ]
   :repositories {"msgpack" "http://msgpack.org/maven2/"}
