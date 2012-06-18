c9d65b4f805388bbb110b58504b134a75e3f78a1
Dave Fayram <dfayram@gmail.com>
2012-06-18 15:38:52 -0700

    Cleanup project file in case you actually want an uberjar.

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "Messagepack serialization library for Clojure."
   :dependencies [
     [org.clojure/clojure "1.4.0"]
-    [org.msgpack/msgpack "0.6.6"]
+    [org.msgpack/msgpack "0.6.6" :exclusions [junit]]
     ]
   :repositories {"msgpack" "http://msgpack.org/maven2/"}
+  :dev-dependnecies [[junit/junit "4.8.2"]]
   )
